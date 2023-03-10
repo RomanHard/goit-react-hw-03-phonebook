5276ff7f47f03f1a5f82e072baff9acc230ea702
Roman <romeenice@gmail.com>
2023-03-10 20:31:41 +0200

    app.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,32 @@ import React from 'react';
 import ContactForm from './contactForm/ContactForm';
 import Filter from './filter/Filter';
 import ContactList from './contactList/ContactList';
-
 export default class App extends React.Component {
   state = {
     contacts: [],
     filter: '',
   };
 
-  handleFilterChange = (filter) => {
+  componentDidMount() {
+    const contactsFromStorage = localStorage.getItem('contacts');
+    if (contactsFromStorage) {
+      this.setState({ contacts: JSON.parse(contactsFromStorage) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  handleFilterChange = filter => {
     this.setState({ filter });
   };
 
-  handleAddContact = (contact) => {
+  handleAddContact = contact => {
     const existingContact = this.state.contacts.find(
-      (c) => c.name === contact.name
+      c => c.name === contact.name
     );
 
     if (existingContact) {
@@ -23,21 +35,21 @@ export default class App extends React.Component {
       return;
     }
 
-    this.setState((prevState) => ({
+    this.setState(prevState => ({
       contacts: [...prevState.contacts, contact],
     }));
   };
 
-  handleDeleteContact = (id) => {
-    this.setState((prevState) => ({
-      contacts: prevState.contacts.filter((c) => c.id !== id),
+  handleDeleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(c => c.id !== id),
     }));
   };
 
   render() {
     const { contacts, filter } = this.state;
     const normalizedFilter = filter.toLowerCase();
-    const filteredContacts = contacts.filter((c) =>
+    const filteredContacts = contacts.filter(c =>
       c.name.toLowerCase().includes(normalizedFilter)
     );
 
